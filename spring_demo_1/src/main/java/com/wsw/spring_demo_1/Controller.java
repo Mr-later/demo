7b59f0f8ac0899fb5ccdf174effd2deca0b72c74
Mr-latter <1947763026@qq.com>
2018-11-18 21:14:10 +0800

    整合jdbcTemplate进去

--- spring_demo_1/src/main/java/com/wsw/spring_demo_1/Controller.java
+++ spring_demo_1/src/main/java/com/wsw/spring_demo_1/Controller.java
@@ -1,30 +1,45 @@
 package com.wsw.spring_demo_1;
 
+import com.wsw.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class Controller {
+
     private static final Logger log = LoggerFactory.getLogger(Controller.class);
     private   final  MyProperties1 myProperties;
     private   final  MyProperties2 myProperties2;
-    public Controller( MyProperties1 myProperties,MyProperties2 myProperties2){
+    private JdbcTemplate jdbcTemplate;
+    public Controller(MyProperties1 myProperties, MyProperties2 myProperties2, JdbcTemplate jdbcTemplate){
         this.myProperties=myProperties;
         this.myProperties2=myProperties2;
+        this.jdbcTemplate=jdbcTemplate;
     }
+
+    /**
+     * 配置文件中的数据得到的实体类myProperties，ConfigurationProperties
+     * @return
+     */
     @RequestMapping("/hello")
     public String hello(){
    System.out.println("进入到hello方法");
         return  "myProperties:"+myProperties+"  myProperties2="+myProperties2;
     }
 
+    /**
+     * thymeleaf和ModelAndView的结合使用
+     * @return
+     */
     @RequestMapping("/index")
      public ModelAndView index(){
          ModelAndView view = new ModelAndView();
@@ -83,4 +98,44 @@ public class Controller {
          }
      }
 
+
+     @RequestMapping("/getAllUser")
+     public   List<User> getAllUser(){
+        String sql ="SELECT * from t_user;";
+        return   jdbcTemplate.queryForList(sql, null, User.class);
+     }
+
+     @RequestMapping("/getUserById")
+     public User getUserById(@PathVariable  Long Id){
+         String sql ="SELECT * from t_user where id =? ";
+         return  jdbcTemplate.queryForList(sql, new Object[]{Id}, User.class).get(0);
+     }
+     @RequestMapping("/deleteUserById")
+     public  List<User> deleteUserById(Long Id){
+         String sql ="delete from t_user where id =? ";
+         jdbcTemplate.update(sql,Id);
+         return   jdbcTemplate.queryForList(sql, null, User.class);
+     }
+
+     @RequestMapping("/addUser")
+     public  List<User> addUser(User user){
+        System.out.println("addUser-------");
+         String sql = "insert into t_user(username, password) values(?, ?)";
+          jdbcTemplate.update(sql,new Object[]{user.getUsername(), user.getPassword()});
+         return   jdbcTemplate.query("SELECT * from t_user ;",
+                 new Object[]{}, new BeanPropertyRowMapper<>(User.class));
+     }
+
+     @RequestMapping("/{id}")
+     public List<User> editUser(@PathVariable long id ,@RequestBody User user ){
+         // 根据主键ID修改用户信息
+         System.out.println(id);
+         System.out.println( user.getUsername());
+         System.out.println(user.getPassword());
+         String sql = "UPDATE t_user SET username = ? ,password = ? WHERE id = ?";
+          jdbcTemplate.update(sql, user.getUsername(), user.getPassword(), id);
+         return   jdbcTemplate.queryForList("SELECT * from t_user where id=?;",
+                 new Object[]{id}, User.class);
+
+     }
 }
